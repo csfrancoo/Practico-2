fe52aca467853325db142d17e94ddde2beda2966
csfrancoo <cayofranco9@gmail.com>
2025-08-13 15:25:48 -0300

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,11 @@
+//creamos la funcion para Cerrar sesion 
+function CerrarSesion(){
+    alert("Cerraste sesion, gracias por la visita")
+}
+
+//funcion para salir de la pagina
+function SalirPagina(){
+
+    //me lleva a la pagina de Google
+    location.href = "https://dti.mendoza.edu.ar/gem/"
+}
